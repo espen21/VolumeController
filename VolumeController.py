1a51b7ff94881ee43f73ddddb6152889230659f9
espen21 <31931020+espen21@users.noreply.github.com>
2023-03-31 00:58:21 +0200

    bugfix

--- VolumeController.py
+++ VolumeController.py
@@ -74,13 +74,15 @@ def play_unmuted():
 # Mute the microphone ,1 = MUTE , 0 = Unmute mic
 def toggle_mic(is_muted):
     if(is_muted):
-        mic_volume.SetMute(0, None)
         print("UnMuted")
         play_unmuted()
+        mic_volume.SetMute(0, None)
+        
     else:
-        mic_volume.SetMute(1, None)
         print("Muted")
         play_muted()
+        mic_volume.SetMute(1, None)
+      
     return  not is_muted
 
 def set_IS_muted():
@@ -89,28 +91,37 @@ def set_IS_muted():
         return False
     return True
 
+
+
 is_muted = set_IS_muted()
 MAX_VOL = 0 # högsta
 MIN_VOL = -65 #
-CURRENT_VOLUME = volume.GetMasterVolumeLevel()
+CURRENT_VOLUME = int(volume.GetMasterVolumeLevel())
 print("Key binds\nUn/mute MIC:[Alt Gr]+[p]\nInCREASE VOLUME: [Alt Gr]+[i]\nDECREASE VOLUME: [Alt Gr]+[o]")
 while(True):
     # Mic
     time.sleep(0.01)
     if(keyboard.is_pressed("Alt Gr+p")):
-        is_muted = toggle_mic(is_muted)
-        time.sleep(1)
-    
+        try:
+            is_muted = toggle_mic(is_muted)
+            time.sleep(1)
+        except Exception as e:
+            print(e)
     #InCREASE VOLUME
     if(keyboard.is_pressed("Alt Gr+i")):
         if CURRENT_VOLUME < MAX_VOL:
-            time.sleep(0.01)
-            CURRENT_VOLUME += 1
-            volume.SetMasterVolumeLevel(CURRENT_VOLUME,None)
+            try:
+                time.sleep(0.01)
+                CURRENT_VOLUME += 1
+                volume.SetMasterVolumeLevel(CURRENT_VOLUME,None)
+            except Exception as e:
+                print(e)
      #DECREASE VOLUME
     if(keyboard.is_pressed("Alt Gr+o")):
         if CURRENT_VOLUME >MIN_VOL:
-            time.sleep(0.01)
-            CURRENT_VOLUME -= 1
-            volume.SetMasterVolumeLevel(CURRENT_VOLUME,None)
-    
+            try:
+                time.sleep(0.01)
+                CURRENT_VOLUME -= 1
+                volume.SetMasterVolumeLevel(CURRENT_VOLUME,None)
+            except Exception as e:
+                print(e)
